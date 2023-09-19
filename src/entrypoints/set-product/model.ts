f82afa5f2d3e2effbe59aef8ba7a0cd687cf21f6
Sobolev515 <alex.sobolev515@yandex.ru>
2023-09-19 13:49:21 +0500

    feat: BIKDARTEM-261. [ARCHITECTURE] Создать ручку /products

---
+++ src/entrypoints/set-product/model.ts
@@ -0,0 +1,18 @@
+﻿import {z} from 'zod';
+
+export const RequestParams = z.object({
+    product_id: z.string(),
+    category_id: z.string(),
+    name: z.string(),
+    description: z.string().optional(),
+    material: z.string().optional(),
+    packaging: z.string().optional(),
+    delivery: z.string().optional(),
+    //TODO: Поменять на enum как станут известны поля 
+    choosing_size_guide: z.string().optional(),
+    price: z.number().positive(),
+    size: z.enum(["S","M","L"]),
+    count: z.number().positive()
+});
+
+export type RequestParamsType = z.infer<typeof RequestParams>;
