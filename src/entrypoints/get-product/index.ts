7850af0cdf7a7970965e47cdf042e7da73d19f19
Sobolev515 <alex.sobolev515@yandex.ru>
2023-09-20 14:59:25 +0500

    BIKDARTEM-263: [ARCHITECTURE] Создать ручку GET /product

---
+++ src/entrypoints/get-product/index.ts
@@ -0,0 +1,21 @@
+﻿import {Handler} from '@yandex-cloud/function-types';
+
+import {requestFromDB} from '_utils/db';
+
+import {RequestParams} from './model';
+import {createDbQuery} from './query';
+
+export const handler: Handler.Http = async function (_event, context) {
+    const data = context.getPayload();
+    const request = RequestParams.parse(data);
+
+    const ydbQuery = createDbQuery(request);
+    const [products, storages] = await requestFromDB(ydbQuery);
+
+    const result = Object.assign({}, products[0], {storage: storages});
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({result})
+    };
+};
