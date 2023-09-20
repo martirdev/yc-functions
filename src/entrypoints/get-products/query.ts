90277cd581657292555acfce766ae54dba4a0f0c
Sobolev515 <alex.sobolev515@yandex.ru>
2023-09-20 14:00:52 +0500

    new func

---
+++ src/entrypoints/get-products/query.ts
@@ -0,0 +1,21 @@
+﻿import {RequestParamsType} from './model';
+
+export const createDbQuery = ({filter, category, limit = 20, offset = 0}: RequestParamsType) => `
+    DECLARE $limit AS Uint32;
+    DECLARE $offset AS Uint32;
+    DECLARE $filter AS String;
+    DECLARE $category AS String;
+
+    $filter = "${filter || ''}";
+    $category = "${category || ''}";
+    $limit = ${limit};
+    $offset = ${offset};
+
+    SELECT * FROM \`products\` as p
+    WHERE
+        IF ($filter = "", true, FIND(p.name, $filter) IS NOT NULL)
+        AND IF ($category = "", true, p.category_id = $category)
+    ORDER BY location ASC
+    LIMIT $limit
+    OFFSET $offset;
+`;
