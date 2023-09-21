f6c2c767d9be1bc517c5b3d71769308721bb82d0
Sobolev515 <alex.sobolev515@yandex.ru>
2023-09-21 14:05:18 +0500

    feat: BIKDARTEM-262: [ARCHITECTURE] Реализовать ручку /products (#6)

---
+++ src/entrypoints/get-products/model.ts
@@ -0,0 +1,10 @@
+﻿import {z} from 'zod';
+
+export const RequestParams = z.object({
+    filter: z.string().optional(),
+    category: z.string().optional(),
+    limit: z.number().positive(),
+    offset: z.number().positive()
+});
+
+export type RequestParamsType = z.infer<typeof RequestParams>;
