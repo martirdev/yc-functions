9e02824a956c40c100b3470d0fc9c34a10f71aad
Sobolev515 <alex.sobolev515@yandex.ru>
2023-09-21 14:05:46 +0500

    feat: BIKDARTEM-263: [ARCHITECTURE] Создать ручку GET /product (#7)

---
+++ src/entrypoints/get-product/query.ts
@@ -0,0 +1,10 @@
+﻿import {RequestParamsType} from './model';
+
+export const createDbQuery = ({product_id}: RequestParamsType) => `
+    DECLARE $product_id AS String;
+
+    $product_id = "${product_id}";
+
+    SELECT * FROM \`products\` WHERE product_id = $product_id;
+    SELECT size, count FROM \`storage\` WHERE product_id = $product_id;
+`;
