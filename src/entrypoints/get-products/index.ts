418155a40a5bcfb92af581ee86225bcadc01501a
Sobolev515 <alex.sobolev515@yandex.ru>
2023-09-26 12:48:55 +0500

    add photos and storage by request

--- src/entrypoints/get-products/index.ts
+++ src/entrypoints/get-products/index.ts
@@ -1,10 +1,24 @@
-﻿import {z} from 'zod';
+﻿import {Handler} from '@yandex-cloud/function-types';
 
-export const RequestParams = z.object({
-    filter: z.string().optional(),
-    type: z.string().optional(),
-    limit: z.number(),
-    offset: z.number()
-});
+import {requestFromDB} from '_utils/db';
 
-export type RequestParamsType = z.infer<typeof RequestParams>;
+import {RequestParams} from './model';
+import {createDbQuery, createDbQueryByStorageAndPhoto} from './query';
+
+export const handler: Handler.Http = async function (_event, context) {
+  const data = context.getPayload();
+  const request = RequestParams.parse(data);
+
+  const ydbQuery = createDbQuery(request);
+  const results = [];
+  const [result] = await requestFromDB(ydbQuery);
+  for (const value of result) {
+    const query = createDbQueryByStorageAndPhoto(value.product_id);
+    const [storages, photos] = await requestFromDB(query);
+    results.push(Object.assign({}, value, {storage: storages}, {photos: photos}));
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(results)
+  };
+};
--- src/entrypoints/get-products/query.ts
+++ src/entrypoints/get-products/query.ts
@@ -11,11 +11,20 @@ export const createDbQuery = ({filter, category, limit = 20, offset = 0}: Reques
     $limit = ${limit};
     $offset = ${offset};
 
-    SELECT * FROM \`products\` as p
+    SELECT * WITHOUT create_at FROM \`products\` as p
     WHERE
         IF ($filter = "", true, FIND(p.name, $filter) IS NOT NULL)
         AND IF ($category = "", true, p.category_id = $category)
     ORDER BY create_at ASC
     LIMIT $limit
     OFFSET $offset;
+`;
+
+export const createDbQueryByStorageAndPhoto = product_id => `
+    DECLARE $product_id AS String;
+
+    $product_id = "${product_id}";
+
+    SELECT size, count FROM storage WHERE product_id = $product_id;
+    SELECT position, link FROM photos WHERE product_id = $product_id;
 `;
