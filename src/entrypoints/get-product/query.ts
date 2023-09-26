212e4ab074bb86e92c625b7ba8998013d4be709d
Sobolev515 <alex.sobolev515@yandex.ru>
2023-09-26 12:49:21 +0500

    remove create_at from answer

--- src/entrypoints/get-product/query.ts
+++ src/entrypoints/get-product/query.ts
@@ -5,7 +5,7 @@ export const createDbQuery = ({product_id}: RequestParamsType) => `
 
     $product_id = "${product_id}";
 
-    SELECT * FROM \`products\` WHERE product_id = $product_id;
+    SELECT * WITHOUT create_at FROM \`products\` WHERE product_id = $product_id;
     SELECT size, count FROM \`storage\` WHERE product_id = $product_id;
     SELECT position, link FROM \`photos\` WHERE product_id = $product_id;
 `;
