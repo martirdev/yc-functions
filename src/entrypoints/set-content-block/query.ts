6b5fc03627f91ab0a58dbd0f2bd6a3a5d177293d
Sobolev515 <alex.sobolev515@yandex.ru>
2023-09-27 13:58:03 +0500

    BIKDARTEM-271: [ARCHITECTURE] Создать ручку /content-block для создания и редактирования

---
+++ src/entrypoints/set-content-block/query.ts
@@ -0,0 +1,17 @@
+﻿import {RequestParamsType} from './model';
+
+export const createDbQuery = ({block_id, section_id, name, content}: RequestParamsType) => `
+    DECLARE $block_id AS String;
+    DECLARE $section_id AS String;
+    DECLARE $name AS String;
+    DECLARE $content AS String;
+
+    $block_id = "${block_id}";
+    $section_id = "${section_id}";
+    $name = ${name};
+    $content = ${content};
+
+    -- Create/Update content block
+    UPSERT INTO \`content-blocks\` (block_id, section_id, name, content)
+    VALUES ($block_id, $section_id, $name, $content);
+`;
