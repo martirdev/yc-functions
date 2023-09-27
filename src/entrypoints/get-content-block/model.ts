c0b3a3c6254f61386e09e3f0b551fa29bbfbe65a
Sobolev515 <alex.sobolev515@yandex.ru>
2023-09-27 15:09:32 +0500

    BIKDARTEM-272: [ARCHITECTURE] Создать ручку GET /content-block

---
+++ src/entrypoints/get-content-block/model.ts
@@ -0,0 +1,7 @@
+﻿import {z} from 'zod';
+
+export const RequestParams = z.object({
+  block_id: z.string()
+});
+
+export type RequestParamsType = z.infer<typeof RequestParams>;
---
+++ src/entrypoints/get-content-block/query.ts
@@ -0,0 +1,9 @@
+﻿import {RequestParamsType} from './model';
+
+export const createDbQuery = ({block_id}: RequestParamsType) => `
+    DECLARE $block_id AS String;
+
+    $block_id = "${block_id}";
+
+    SELECT * FROM \`content-blocks\` WHERE block_id = $block_id;
+`;
