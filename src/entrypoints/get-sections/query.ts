a85329bf90ae16faf2b382cd898a311e68899387
Sobolev515 <alex.sobolev515@yandex.ru>
2023-09-28 14:37:54 +0500

    base code

---
+++ src/entrypoints/get-sections/query.ts
@@ -0,0 +1,9 @@
+﻿import {RequestParamsType} from './model';
+
+export const createDbQuery = ({parent_id}: RequestParamsType) => `
+    DECLARE $parent_id AS String;
+
+    $parent_id = "${parent_id}";
+
+    SELECT * FROM \`sections\` WHERE parent_id = $parent_id;
+`;
