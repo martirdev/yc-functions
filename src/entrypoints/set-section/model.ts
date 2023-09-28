45f3404471072c15bcaad2712894f14920a44721
Sobolev515 <alex.sobolev515@yandex.ru>
2023-09-28 15:31:40 +0500

    base code

---
+++ src/entrypoints/set-section/model.ts
@@ -0,0 +1,9 @@
+﻿import {z} from 'zod';
+
+export const RequestParams = z.object({
+  section_id: z.string(),
+  parent_id: z.string().optional(),
+  name: z.string()
+});
+
+export type RequestParamsType = z.infer<typeof RequestParams>;
---
+++ src/entrypoints/set-section/query.ts
@@ -0,0 +1,15 @@
+﻿import {RequestParamsType} from './model';
+
+export const createDbQuery = ({section_id, parent_id, name}: RequestParamsType) => `
+    DECLARE $parent_id AS String;
+    DECLARE $section_id AS String;
+    DECLARE $name AS String;
+
+    $section_id = "${section_id}";
+    $parent_id = "${parent_id || ''}";
+    $name = "${name}";
+
+    -- Create/Update section
+    UPSERT INTO \`sections\` (section_id, parent_id, name)
+    VALUES ($section_id, $parent_id, $name);
+`;
