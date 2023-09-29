3b41cffb174d810bc52145b1fc1cb2095e7ef132
Sobolev515 <alex.sobolev515@yandex.ru>
2023-09-29 20:25:24 +0500

    Bikdartem 271 architecture] создать ручку /content block для создания и редактирования (#11)

---
+++ src/entrypoints/set-content-block/model.ts
@@ -0,0 +1,10 @@
+﻿import {z} from 'zod';
+
+export const RequestParams = z.object({
+  block_id: z.string(),
+  name: z.string(),
+  section_id: z.string(),
+  content: z.string().optional()
+});
+
+export type RequestParamsType = z.infer<typeof RequestParams>;
