26468c8e953dc3ca72a0c8c9a1893fd4431cc7fa
Sobolev515 <alex.sobolev515@yandex.ru>
2023-10-06 12:30:21 +0500

    BIKDARTEM-279: [ARCHITECTURE] Ручка на получение задачи

---
+++ src/entrypoints/set-task/model.ts
@@ -0,0 +1,16 @@
+﻿import {z} from 'zod';
+
+export const RequestParams = z.object({
+  task_id: z.string(),
+  author_id: z.string(),
+  executor_id: z.string().optional(),
+  name: z.string(),
+  context: z.string().optional(),
+  task: z.string().optional(),
+  status_task: z.enum(['Открыт', 'В работе', 'Ждет проверки', 'Закрыт', 'Требуется информация', 'В беклог']),
+  planned_sp: z.number().positive().optional(),
+  spent_sp: z.number().positive().optional(),
+  close_at: z.date().optional()
+});
+
+export type RequestParamsType = z.infer<typeof RequestParams>;
---
+++ src/entrypoints/set-task/query.ts
@@ -0,0 +1,60 @@
+﻿import {RequestParamsType} from './model';
+
+export const createDbQuery = ({
+  task_id,
+  author_id,
+  executor_id,
+  name,
+  content,
+  task,
+  status_task,
+  planned_sp,
+  spent_sp,
+  type_task_id,
+  close_at
+}: RequestParamsType) => `
+    DECLARE $task_id AS String;
+    DECLARE $section_id AS String;
+    DECLARE $executor_id AS String;
+    DECLARE $name AS String;
+    DECLARE $content AS String;
+    DECLARE $task AS String;
+    DECLARE $status_task AS String;
+    DECLARE $planned_sp AS Uint64;
+    DECLARE $spent_sp AS Uint64;
+    DECLARE $type_task_id AS String;
+    DECLARE $close_at AS DateTime;
+
+    $task_id = "${task_id}";
+    $author_id = "${author_id}";
+    $executor_id = "${executor_id || ''}";
+    $name = "${name}";
+    $content = "${content || ''}";
+    $task = "${task || ''}";
+    $status_task = "${status_task || 'Открыт'}";
+    $planned_sp = ${planned_sp || null};
+    $spent_sp = ${spent_sp || null};
+    $type_task_id = "${type_task_id || ''}";
+    $close_at = ${close_at || null};
+
+    
+    -- Create/check create_at
+    $create_at = (SELECT create_at FROM \`tasks\` WHERE task_id = $task_id);
+    
+    -- Create/Update tasks
+    UPSERT INTO \`tasks\` (task_id, author_id, executor_id, name, content, task, status_task, planned_sp,
+    spent_sp, create_at, update_at, type_task_id, close_at)
+    VALUES ($task_id, $author_id, $executor_id, $name, $content, $task, $status_task, $planned_sp, $spent_sp, 
+    IF($create_at IS NULL, CurrentUtcDatetime(), $create_at), CurrentUtcDatetime(), $type_task_id, $close_at);
+`;
+
+export const createDbQueryAddWatcher = (task_id, user_id) => `
+    DECLARE $task_id AS String;
+    DECLARE $user_id AS String;
+    
+    $task_id = "${task_id}";
+    $user_id = "${user_id}";
+    -- Create/Update watchers author
+    UPSERT INTO \`watchers\` (task_id, user_id)
+    VALUES ($task_id, $user_id);
+`;
