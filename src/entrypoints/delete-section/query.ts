eab8899e6f57b3029f953a94c90e353902e8d8c4
Sobolev515 <alex.sobolev515@yandex.ru>
2023-10-06 12:30:40 +0500

    feat: BIKDARTEM-290: [ARCHITECTURE] Реализовать ручку DELETE: /content-service/section (#14)

---
+++ src/entrypoints/delete-section/query.ts
@@ -0,0 +1,8 @@
+﻿import {RequestParamsType} from './model';
+
+export const createDbQuery = ({section_id}: RequestParamsType) => `
+DECLARE $product_id AS String;
+$section_id = "${section_id}";
+Delete from \`sections\`
+Where section_id=$section_id
+`;
