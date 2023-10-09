65f05445de8640e1defe779796ff4a9115532e2e
Sobolev515 <alex.sobolev515@yandex.ru>
2023-10-09 13:24:47 +0500

    BIKDARTEM-278: [ARCHITECTURE] Ручка на получение списка задач

---
+++ src/entrypoints/get-tasks/query.ts
@@ -0,0 +1,21 @@
+﻿import {RequestParamsType} from './model';
+
+export const createDbQuery = ({filter, limit = 20, offset = 0}: RequestParamsType) => `
+    DECLARE $limit AS Uint32;
+    DECLARE $offset AS Uint32;
+    DECLARE $filter AS String;
+
+    $filter = ${filter || '1972-01-01'};
+    $limit = ${limit};
+    $offset = ${offset};
+
+    SELECT * FROM \`tasks\` as c
+    WHERE
+        IF ($filter = "1972-01-01",
+            true,
+            close_at < Date($filter)
+        )
+    ORDER BY create_at ASC
+    LIMIT $limit
+    OFFSET $offset;
+`;
