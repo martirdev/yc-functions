fa956aafc99b775a451bf37c14456c90a84e4f49
Sobolev515 <alex.sobolev515@yandex.ru>
2023-10-09 14:03:14 +0500

    BIKDARTEM-279: [ARCHITECTURE] Ручка на получение задачи

---
+++ src/entrypoints/get-task/index.ts
@@ -0,0 +1,18 @@
+﻿import {Handler} from '@yandex-cloud/function-types';
+
+import {requestFromDB} from '_utils/db';
+
+import {RequestParams} from './model';
+import {createDbQuery} from './query';
+
+export const handler: Handler.Http = async function (_event, context) {
+    const data = context.getPayload();
+    const request = RequestParams.parse(data);
+
+    const ydbQuery = createDbQuery(request);
+    const result = await requestFromDB(ydbQuery);
+    return {
+        statusCode: 200,
+        body: JSON.stringify({"result":result[0][0]})
+    };
+};
