9458251e615ef2e1f43802989d49a45a1483bfa8
Sobolev515 <alex.sobolev515@yandex.ru>
2023-10-09 14:18:45 +0500

    BIKDARTEM-277: [ARCHITECTURE] Ручка на создание/редактирование задач (#23)

---
+++ src/entrypoints/set-task/index.ts
@@ -0,0 +1,28 @@
+﻿import {Handler} from '@yandex-cloud/function-types';
+
+import {requestFromDB} from '_utils/db';
+
+import {RequestParams} from './model';
+import {createDbQuery, createDbQueryAddWatcher} from './query';
+
+export const handler: Handler.Http = async function (_event, context) {
+  const data = context.getPayload();
+  const request = RequestParams.parse(data);
+  const ydbQuery = createDbQuery(request);
+  await requestFromDB(ydbQuery)
+
+  if (request.author_id.toString() != ''){
+    const query = createDbQueryAddWatcher(request.task_id, request.author_id);
+    await requestFromDB(query);
+    
+  }
+  if (request.executor_id.toString() != ''){
+    const query = createDbQueryAddWatcher(request.task_id, request.executor_id);
+    await requestFromDB(query);
+  }
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify('Ok')
+  };
+};
