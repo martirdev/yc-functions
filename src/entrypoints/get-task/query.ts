15e352723b3cec1c444a31765d0d3157a5b4caf7
Sobolev515 <alex.sobolev515@yandex.ru>
2023-10-09 14:18:54 +0500

    Bikdartem 279 architecture] ручка на получение задачи (#25)

---
+++ src/entrypoints/get-task/query.ts
@@ -0,0 +1,8 @@
+﻿import {RequestParamsType} from './model';
+
+export const createDbQuery = ({task_id}: RequestParamsType) => `
+    DECLARE $task_id AS String;
+
+    $task_id = "${task_id}";
+    SELECT * FROM \`tasks\` WHERE task_id = $task_id;
+`;
