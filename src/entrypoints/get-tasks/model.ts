893cd511a9fbf463dd557b808d01fff76b8d12b0
Sobolev515 <alex.sobolev515@yandex.ru>
2023-10-09 14:18:36 +0500

    BIKDARTEM-278: [ARCHITECTURE] Ручка на получение списка задач (#24)

---
+++ src/entrypoints/get-tasks/model.ts
@@ -0,0 +1,9 @@
+﻿import {z} from 'zod';
+
+export const RequestParams = z.object({
+    filter: z.string().optional(),
+    limit: z.number(),
+    offset: z.number()
+});
+
+export type RequestParamsType = z.infer<typeof RequestParams>;
