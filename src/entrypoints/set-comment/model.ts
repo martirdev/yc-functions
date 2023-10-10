f3136aefef20088e730a09ef094662820f531f56
Sobolev515 <alex.sobolev515@yandex.ru>
2023-10-10 11:37:37 +0500

    BIKDARTEM-281: [ARCHITECTURE] Ручка на создание/редактирования коммен… (#27)

---
+++ src/entrypoints/set-comment/model.ts
@@ -0,0 +1,10 @@
+﻿import {z} from 'zod';
+
+export const RequestParams = z.object({
+    comment_id: z.string(),
+    task_id: z.string(),
+    user_id: z.string(),
+    context: z.string(),
+});
+
+export type RequestParamsType = z.infer<typeof RequestParams>;
---
+++ src/entrypoints/set-comment/query.ts
@@ -0,0 +1,21 @@
+﻿import {RequestParamsType} from './model';
+
+export const createDbQuery = ({comment_id, task_id, user_id, context, create_at, update_at}: RequestParamsType) => `
+    DECLARE $comment_id AS String;
+    DECLARE $task_id AS String;
+    DECLARE $user_id AS String;
+    DECLARE $context AS String;
+
+    $comment_id = "${comment_id}";
+    $task_id = "${task_id}";
+    $user_id = "${user_id}";
+    $context = "${context}";
+    
+    -- Create/check create_at
+    $create_at = (SELECT create_at FROM \`comments\` WHERE comment_id = $comment_id);
+    
+    -- Create/Update comments
+    UPSERT INTO \`comments\` (comment_id, task_id, user_id, context, create_at, update_at)
+    VALUES ($comment_id, $task_id, $user_id, $context, IF($create_at IS NULL, CurrentUtcDatetime(), $create_at),
+     CurrentUtcDatetime());
+`;
