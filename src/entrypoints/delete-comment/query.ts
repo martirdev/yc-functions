1f73bd954ec4f16fd9cf3ba810da7ea4dfa37cda
Sobolev515 <alex.sobolev515@yandex.ru>
2023-10-10 12:21:47 +0500

    BIKDARTEM-282: [ARCHITECTURE] Ручка на удаление комментария к задаче

---
+++ src/entrypoints/delete-comment/query.ts
@@ -0,0 +1,8 @@
+﻿import {RequestParamsType} from './model';
+
+export const createDbQuery = ({comment_id}: RequestParamsType) => `
+DECLARE $comment_id AS String;
+$comment_id = "${comment_id}";
+Delete from \`comments\`
+Where comment_id=$comment_id
+`;
