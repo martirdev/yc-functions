c7631a31632becd7abf34dc977cbe47f56a9916a
Sobolev515 <alex.sobolev515@yandex.ru>
2023-10-16 20:58:23 +0500

    BIKDARTEM-312: [ARCHITECTURE] Ручка на редактирование заказа (Статуса)

---
+++ src/entrypoints/set-order-status/query.ts
@@ -0,0 +1,13 @@
+﻿import {RequestParamsType} from './model';
+
+export const createDbQuery = ({order_id, status}: RequestParamsType) => `
+    DECLARE $order_id AS String;
+    DECLARE $status AS String;
+    
+    $order_id = "${order_id}";
+    $status = "${status}";
+
+    -- Create orders-status info
+    INSERT  INTO \`orders-status\` (order_status_id, order_id, status, date)
+    VALUES (CAST(RandomUuid(1234) AS String), $order_id, $status, CurrentUtcDatetime());
+`;
