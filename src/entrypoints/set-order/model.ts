07dc0f1802dcbbd58e99b55d5ac1dc9f9689feb4
Sobolev515 <alex.sobolev515@yandex.ru>
2023-10-16 20:16:23 +0500

    BIKDARTEM-311: [ARCHITECTURE] Ручка на создание заказа

---
+++ src/entrypoints/set-order/model.ts
@@ -0,0 +1,28 @@
+﻿import {z} from 'zod';
+
+export const RequestParams = z.object({
+    order_id: z.string(),
+    delivery: z.string(),
+    full_name_customer: z.string(),
+    email: z.string().optional(),
+    phone: z.string(),
+    region: z.string(),
+    city: z.string(),
+    street: z.string(),
+    house: z.string(),
+    flat: z.string(),
+    index: z.string(),
+    products: z
+        .array(
+            z.object({
+                product_id: z.string(),
+                size: z.enum(['NoSize', 'XS', 'S', 'M', 'L', 'XL']),
+                count: z.number().positive(),
+                total_price: z.number()
+            })
+        )
+        .optional(),
+});
+
+export type RequestParamsType = z.infer<typeof RequestParams>;
+export type RequestProductsParamsType = z.infer<typeof RequestParams>['products'];
