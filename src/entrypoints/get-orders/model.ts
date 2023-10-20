524e5a34ab3bd3e65d6d04e284a077a5a49915d1
Sobolev515 <alex.sobolev515@yandex.ru>
2023-10-20 16:33:20 +0500

    BIKDARTEM-309: [ARCHITECTURE] Ручка на получение списка заказов

---
+++ src/entrypoints/get-orders/model.ts
@@ -0,0 +1,7 @@
+﻿import {z} from 'zod';
+
+export const RequestParams = z.object({
+    order_id: z.string()
+});
+
+export type RequestParamsType = z.infer<typeof RequestParams>;
---
+++ src/entrypoints/get-orders/query.ts
@@ -0,0 +1,9 @@
+﻿import {RequestParamsType} from './model';
+
+export const createDbQuery = ({order_id}: RequestParamsType) => `
+    DECLARE $order_id AS String;
+
+    $order_id = "${order_id}";
+
+    SELECT * FROM \`orders\` WHERE order_id = $order_id;
+`;
