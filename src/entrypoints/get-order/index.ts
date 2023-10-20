820def65cb343694504e3be90e8c652bbe0e864e
Sobolev515 <alex.sobolev515@yandex.ru>
2023-10-20 16:28:55 +0500

    BIKDARTEM-310: [ARCHITECTURE] Ручка на получение информации заказа

---
+++ src/entrypoints/get-order/index.ts
@@ -0,0 +1,21 @@
+﻿import {Handler} from '@yandex-cloud/function-types';
+
+import {requestFromDB} from '_utils/db';
+
+import {RequestParams} from './model';
+import {createDbQuery} from './query';
+
+export const handler: Handler.Http = async function (_event, context) {
+    const data = context.getPayload();
+    const request = RequestParams.parse(data);
+
+    const ydbQuery = createDbQuery(request);
+    const [orders, status, products] = await requestFromDB(ydbQuery);
+
+    const result = Object.assign(orders[0], status[0], {products: products});
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({result})
+    };
+};
---
+++ src/entrypoints/get-order/query.ts
@@ -0,0 +1,11 @@
+﻿import {RequestParamsType} from './model';
+
+export const createDbQuery = ({order_id}: RequestParamsType) => `
+    DECLARE $order_id AS String;
+
+    $order_id = "${order_id}";
+
+    SELECT * FROM \`orders\` WHERE order_id = $order_id;
+    SELECT status FROM \`orders-status\` WHERE order_id = $order_id Order by date;
+    SELECT * Without order_id FROM \`orders-products\` WHERE order_id = $order_id;
+`;
