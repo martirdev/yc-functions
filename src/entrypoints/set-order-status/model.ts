36a53e4f5beec99fc4ffa03677989664d78dfb8e
Sobolev515 <alex.sobolev515@yandex.ru>
2023-10-31 12:48:08 +0500

    BIKDARTEM-312: [ARCHITECTURE] Ручка на редактирование заказа (Статуса) (#32)

---
+++ src/entrypoints/set-order-status/model.ts
@@ -0,0 +1,9 @@
+﻿import {z} from 'zod';
+
+export const RequestParams = z.object({
+    order_id: z.string(),
+    status: z.enum(['В обработке', 'Требуется информация', 'В сборке', 'Готов к получению', 'В службе доставки',
+'Заказ выполнен'])
+});
+
+export type RequestParamsType = z.infer<typeof RequestParams>;
