b3f6a1f08f969db6332b24376b82d24f5666c9d4
Sobolev515 <alex.sobolev515@yandex.ru>
2023-10-31 12:48:31 +0500

    BIKDARTEM-280: [ARCHITECTURE] Ручка на получение списка комментариев … (#28)

---
+++ src/entrypoints/get-comments/query.ts
@@ -0,0 +1,11 @@
+﻿import {RequestParamsType} from './model';
+
+export const createDbQuery = ({task_id}: RequestParamsType) => `
+    DECLARE $task_id AS String;
+
+    $task_id = "${task_id}";
+
+    SELECT * FROM \`comments\` 
+    WHERE task_id = $task_id    
+    ORDER BY create_at ASC;
+`;
